d31761cce4343604a219b57671d4e80e34ba2dcd
Brent Brimhall <brent.brimhall@gmail.com>
2016-07-31 02:45:58 -0700

    ch 8 done

--- modules/src/cljs/something.cljs
+++ modules/src/cljs/something.cljs
@@ -1,4 +1,6 @@
-(ns modules.something)
+(ns modules.something
+  ; (:require-macros [... as ...]) ; macros are loaded differently in cljs files.
+  )
 
 (enable-console-print!)
 
@@ -6,3 +8,27 @@
 
 (defn ^:export this-will-not-be-munged-by-goog-closure []
   (println "hi!"))
+
+; Macros!
+
+; (defmacro when2
+;   ([condition & body]
+;    (list 'if condition
+;          (cons 'do body))))
+
+; or with syntax quoting
+(defmacro when2
+  [condition & body]
+  `(if ~condition (do ~@body))) ; ~@ -- unquote splicing: inserts the contents of the list
+
+(when2 true
+       (println "when2!"))
+
+; auto-gensyms: keyword# is guranteed to be unique, it won't clash with anything passed in to the macro.
+(defmacro debug [expr]
+  `(let [result# ~expr]
+     (println "Evaluating:" '~expr) ; quote unquote: so you can print the literal code of expr w/o evaluating it.
+     (println "Result:" result#)
+     result#))
+
+
